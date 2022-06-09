70b34fe945a369b0336f2e99dd4457a1a9cfe1b4
Cade Mirchandani <cademirch@gmail.com>
2022-06-09 16:42:55 -0700

    cool

--- decoder.py
+++ decoder.py
@@ -4,14 +4,14 @@ from random import choice
 
 
 with open(sys.argv[1], "r") as fh:
-    text = b""
+    text = []
     for i, line in enumerate(fh):
         if line.startswith("+"):
             line = next(fh)
             line = line.strip()
-            text += bytes(line, "utf-8")
+            text.append(bytes(line, "utf-8"))
 
-
-fh = open("out.mp3", "wb")
-fh.write(base64.b64decode(text + b"=="))
+out_text = b"".join(text)
+fh = open("out.mp4", "wb")
+fh.write(base64.b64decode(out_text + b"=="))
 fh.close()
